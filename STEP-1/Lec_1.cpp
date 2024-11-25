cd1f2afced1e67aabe602a22d961c67bec5f4947
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-11-25 19:48:22 +0530

    new commit

--- STEP-1/Lec_1.cpp
+++ STEP-1/Lec_1.cpp
@@ -1,2 +1,13 @@
 //User Input/Output
 
+//g++ -o <name you want to give><name of the file> (g++ -0 1 Lec_1.cpp )
+#include <iostream>
+using namespace std;
+
+int main(){
+  int x,y;
+  cin >> x >> y;
+  cout << "Value of x : " << x << "Value of y :"<< y << endl;
+  return 0;
+}
+

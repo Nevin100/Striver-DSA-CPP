ec8812865de8a15dda37d9e251173d5c85b36f7f
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-11-25 19:55:16 +0530

    new commit

--- STEP-1/Lec_1.cpp
+++ STEP-1/Lec_1.cpp
@@ -1,13 +1,14 @@
 //User Input/Output
 
 //g++ -o <name you want to give><name of the file> (g++ -0 1 Lec_1.cpp )
-#include <iostream>
-using namespace std;
 
-int main(){
-  int x,y;
-  cin >> x >> y;
-  cout << "Value of x : " << x << "Value of y :"<< y << endl;
+#include <iostream> //file importing 
+using namespace std;  //using namespace to reduce the ambiguity of standard names and identifires
+
+int main(){ //main function
+  int x,y; //int datatype 
+  cin >> x >> y; //taking x and y value from input
+  cout << "Value of x : " << x << "Value of y :"<< y << endl;  //Ouput of x and y
   return 0;
 }
 

a99553531171fde7c2a023c1bc25fe651a2d12a9
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-11-25 22:27:24 +0530

    new commit

--- STEP-1/Lec_1.cpp
+++ STEP-1/Lec_1.cpp
@@ -18,8 +18,7 @@ int main(){ //main function
 
 //2.) Data Type :Type of data being used in the program.
 
-// C.)Condition
-//3.) Write a program that takes an input of age and check whteher you are an adult or not?
+//3.)Condition if-else
 
 #include <iostream>
 using namespace std;
@@ -30,6 +29,42 @@ int main(){
     cout<<"You are an adult"<<endl;
   }else{
     cout<<"You are not adult"<<endl;
+  }
+  return 0;
+}
+
+//4.) Switch Statement
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  int day;
+  cin >> day;
+  switch(day){
+    case 1:
+      cout << "Monday";
+      break;
+    case 2:
+      cout << "Tuesday";
+      break;
+    case 3:
+      cout << "Wednesday";
+      break;
+    case 4:
+      cout << "Thursday";
+      break;
+    case 5:
+      cout << "Friday";
+      break;
+    case 6:
+      cout << "Saturday";
+      break;
+    case 7:
+      cout << "Sunday";
+      break;
+    default:
+      cout << "Invalid switch";
   }
   return 0;
 }

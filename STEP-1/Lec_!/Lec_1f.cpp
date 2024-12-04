829bb6c0bf0805149d6da743b01d9b65cba76921
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-12-04 23:09:01 +0530

    new commit

---
+++ STEP-1/Lec_!/Lec_1f.cpp
@@ -0,0 +1,22 @@
+//Time Complexity Fundamentals: 
+
+//Time complexity :  the rate at which the running time of an algorithm increases as the size of the input increases.
+
+//1.) Big - O Notation : worst case or upper- bound
+//(Note : time omplexity is calculated in terms of worst case scenerios usually, avoid constants and avoid lower values)
+
+// example : 
+//for(int i =0; i<n; i++){ cout << "Raj"}; ---> O(3n) {n times for loop and 3 times extra for sum (cout statement,check,increment step of i) };
+
+//2.) Theta - Notation : average complexity
+//3.) Omega-Notatiuon : best case complexity
+
+//Space Complexity : space complexity is the amount of space required by an algorithm to run.
+
+//1.)Auxiliary Space : space that you take to solve the problem
+//2.)Input Space : space you took to store the input.
+
+//Example : c = a + b; --->(here c : Auxiliary space and a & b are input space0)
+
+
+//INTERVIEW : Never ever do anything to the input values

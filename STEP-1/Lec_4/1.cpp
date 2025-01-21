0bc772724e3ad767ced1935e28b5dcdba5b500c5
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-21 22:04:44 +0530

    Update Lec4

---
+++ STEP-1/Lec_4/1.cpp
@@ -0,0 +1,21 @@
+//Basic Maths : 
+
+// To extract individual digits go for : %10 to get last and subsequently %10 and again %10 ---> will give second last and so on!!
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout << "Enter the n : "<< endl;
+  cin >> n;
+  while( n > 0 ){
+    int lastDigit = n % 10 ;
+    n = n / 10;
+    cout << "Last digit :" << lastDigit << endl;
+  }
+  cout << endl;
+  return 0;
+}
+

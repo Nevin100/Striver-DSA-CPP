edebbafe5c25890f6afb04b841d6e0817d9f505c
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-21 17:36:30 +0530

    Vectors

--- STEP-1/Lec_3/1.cpp
+++ STEP-1/Lec_3/1.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h> //include all the libraries
+#include <vector>
 using namespace std;
+
 
 int main(){
   //STL -> Standarad Template Library:
+  vector <int> vec;
+  vec.push_back(1); //To push back the element 1 at the end
+  vec.push_back(2); //To push back the element 2 at the end
+  vec.push_back(3); //To push back the element 3 at the end
+
+  cout << vec.size() <<  endl;
+  cout << vec.capacity() <<  endl;
+  return 0;
 }
+
+

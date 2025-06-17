4efb7de7a07594a05299d30977815b3e6e6ce307
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-06-17 12:24:18 +0530

    Setting extra folder and started lrevising previous code

--- STEP-1/Lec_!/Lec_1b.cpp
+++ STEP-1/Lec_!/Lec_1b.cpp
@@ -1,25 +1,25 @@
 // Arrays : It is a linear data homogenous structure where all the elements are stored in contiginuous memory location.
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//   int arr[5];
-//   cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4] ;
-//   cout << arr[4];
-//   return 0; 
-// }
+int main(){
+  int arr[5];
+  cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4] ;
+  cout << arr[4];
+  return 0; 
+}
 
-// //2D Array:
-// #include <iostream>
-// using namespace std;
+//2D Array:
+#include <iostream>
+using namespace std;
 
-// int main(){
-//   int arr[10][10];
-//   arr[1][2] = 28;
-//   cout << arr[1][2];
-//   return 0;
-// }
+int main(){
+  int arr[10][10];
+  arr[1][2] = 28;
+  cout << arr[1][2];
+  return 0;
+}
 
 //Strings : it is an array of charactars.
 

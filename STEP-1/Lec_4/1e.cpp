15a63133239d6310eb581c5add3e54bf523a3eac
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-06-18 19:46:05 +0530

    Completed

--- STEP-1/Lec_4/1e.cpp
+++ STEP-1/Lec_4/1e.cpp
@@ -4,6 +4,22 @@
 using namespace std;
 
 int main(){
+  int n, dup;
+  int sum = 0;
+  cout << "Enter the number : ";
+  cin >> n;
 
+  dup = n;
+  while(n > 0){
+    int lastDigit = n%10;
+    sum += (lastDigit * lastDigit * lastDigit);
+    n /= 10;
+  }
+
+  if(dup == sum){
+    cout << "Armstrong number" << endl;
+  } else {
+    cout << "Not an Armstrong number" << endl;
+  }
   return 0;
 }

f4039c23e7c34c1193bd37f5a5a933b096ef7c16
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-06-18 18:54:58 +0530

    Lec4

--- STEP-1/Lec_4/1b.cpp
+++ STEP-1/Lec_4/1b.cpp
@@ -17,3 +17,17 @@ int main(){
   return 0;
 }
  
+
+// Shortcut or (implementation of Logarithmic code) :
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cout << "Enter the number n :" << endl;
+  cin >> n;
+  int count = 0;
+  count = (int)(log10(n) + 1); // log(n) gives the number of digits in n
+  cout << "The count of digits in n : " << count << endl;
+  return 0;
+}

e85299dbc414263e4f8b8867cd82de72f15fa5a1
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-08-03 14:09:22 +0530

    Started Step 3

---
+++ Extra/Notes-Step-3/imp.cpp
@@ -0,0 +1,4 @@
+// Arrrays  : 
+// Arrays is a data structure which holds multiple values in contigunous locations but of same type 
+
+// In interview , one should drive the interview... must ask the test cases, the complexities and always start answering the questions from the brute force algorithm... which is basically the solution first came into mind.

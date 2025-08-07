94794bb59fbc0a47f504819bb2d9bc1b5961b29f
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-08-07 18:11:48 +0530

    Completing Step-3 Easy

--- STEP-3/Easy/1i.cpp
+++ STEP-3/Easy/1i.cpp
@@ -1,38 +1,75 @@
-// Q.) Union of two sorted arrays :
-
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-  int n1;
-  cout << "Enter the size of array 1 :";
-  cin >> n1;
+int main() {
+    int n1;
+    cout << "Enter the size of array 1: ";
+    cin >> n1;
 
-  int n2;
-  cout << "Enter the size of array 2 :";
-  cin >> n2;
+    int n2;
+    cout << "Enter the size of array 2: ";
+    cin >> n2;
 
-  vector<int> arr1(n1);
-  for (int i = 0; i < n1; i++){
-    cout << "Enter the elements (in sorted manner) at the index " << i << "of the array 1: ";
-    cin >> arr1[i];
-  }
-
-  vector<int> arr2(n2);
-  for (int i = 0; i < n2; i++){
-    cout << "Enter the elements (in sorted manner) at the index " << i << "of the array 2: ";
-    cin >> arr2[i];
-  }
-
-  //Two pointer approach :
-  vector<int> union;
-  for(int i = 0; i < n1; i++ ){
-    for(int j = 0; j < n2; j++){
-      if(arr1[i] < arr2[j]){
-        if(union.back() != a[i])
-      }
+    vector<int> arr1(n1);
+    for (int i = 0; i < n1; i++) {
+        cout << "Enter the elements (in sorted manner) at the index " << i << " of the array 1: ";
+        cin >> arr1[i];
     }
-  }
-  
-  return 0;
+
+    vector<int> arr2(n2);
+    for (int i = 0; i < n2; i++) {
+        cout << "Enter the elements (in sorted manner) at the index " << i << " of the array 2: ";
+        cin >> arr2[i];
+    }
+
+    // Two-pointer approach:
+    vector<int> unionArr;
+    int i = 0, j = 0;
+
+    while (i < n1 && j < n2) {
+        // Skip duplicates in arr1
+        if (i > 0 && arr1[i] == arr1[i - 1]) {
+            i++;
+            continue;
+        }
+        // Skip duplicates in arr2
+        if (j > 0 && arr2[j] == arr2[j - 1]) {
+            j++;
+            continue;
+        }
+
+        if (arr1[i] < arr2[j]) {
+            unionArr.push_back(arr1[i]);
+            i++;
+        } else if (arr1[i] > arr2[j]) {
+            unionArr.push_back(arr2[j]);
+            j++;
+        } else {
+            unionArr.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+    }
+
+    // Add remaining elements from arr1
+    while (i < n1) {
+        if (i == 0 || arr1[i] != arr1[i - 1])
+            unionArr.push_back(arr1[i]);
+        i++;
+    }
+
+    // Add remaining elements from arr2
+    while (j < n2) {
+        if (j == 0 || arr2[j] != arr2[j - 1])
+            unionArr.push_back(arr2[j]);
+        j++;
+    }
+
+    // Print the union
+    cout << "\nUnion of the two arrays is:\n";
+    for (int val : unionArr) {
+        cout << val << " ";
+    }
+
+    return 0;
 }
